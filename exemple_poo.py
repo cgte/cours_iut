270d581c2f9e81f8c7b90cc91602a6c65f096f27
Colin Goutte <cgte@bk.ru>
2020-01-27 22:57:10 +0100

    WIP

--- exemple_poo.py
+++ exemple_poo.py
@@ -61,7 +61,7 @@ print(nouveau_point)
 class Point:
     def __init__(self, x, y):
         self.x = x
-        self.y = x
+        self.y = y
 
     def __str__(self):
         return f"({self.x},{self.y})"
